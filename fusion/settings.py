3bf545a3659102334bc0d8627570cfbf721677e7
Vitor <vitorbeatle@gmail.com>
2023-06-29 16:30:03 -0300

    heroku6

--- fusion/settings.py
+++ fusion/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'fusion.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -88,13 +88,13 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-
-
 '''
+
+
 DATABASES = {
     'default': dj_database_url.config()
 }
-'''
+
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
