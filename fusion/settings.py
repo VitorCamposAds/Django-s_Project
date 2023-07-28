136b0768807a4a07b671f60fb519244ef7fee7a9
VitorCamposAds <vitorbeatle@gmail.com>
2023-07-27 22:15:45 -0300

    Atualizar configuração do banco de dados

--- fusion/settings.py
+++ fusion/settings.py
@@ -100,7 +100,7 @@ DATABASES = {
 }
 '''
 DATABASES = {
-    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+    'default': dj_database_url.config(default=os.environ.get('HEROKU_POSTGRESQL_JADE_URL'))
 }
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
